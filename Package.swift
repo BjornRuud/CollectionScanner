67f46448fc8b0a38b8e201b0beed93b57f149024
Bjørn Olav Ruud <mail@bjornruud.net>
2023-04-23 16:30:46 +0200

    Update swift tools version and platforms.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,16 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
     name: "CollectionScanner",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .macCatalyst(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "CollectionScanner",
